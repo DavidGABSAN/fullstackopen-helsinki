7d6e6b873e5c19d9b7af8b3fbce13f524d32dccb
David Gabriel Santonja Valles <david.santonja89@gmail.com>
2026-08-01 18:42:24 +0200

    Part 2 - 2.5: courseinfo step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,39 +1,4 @@
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((s, part) => s + part.exercises, 0);
-  return <h3>total of {total} exercises</h3>;
-};
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const Courses = ({ courses }) => {
   return (
